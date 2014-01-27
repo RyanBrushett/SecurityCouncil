0fd8a11164cf07feb047d678d0f583303147cfb2
Ryan Brushett <ryanbrushett@gmail.com>
2014-01-27 18:43:11 -0330

    Working on the simulation management page

--- Security_Council_5/routes/scadmin.js
+++ Security_Council_5/routes/scadmin.js
@@ -3,27 +3,19 @@ var users = require('../db').users;
 var Hogan = require('hjs');
 
 exports.getscadmin = function(req,res){
-    var view     = {names: users};
-    var template = "{{#names}}<li>Name: {{name}} - ID: {{id}}</li>{{/names}}";
-    var compiled = Hogan.compile(template);
-    var html     = compiled.render(view);
     res.render('sc-admin', {
-                title: 'Admin Page',
-                userlist: html
+                title: 'Admin Page'
     });
 };
 
 // Demonstrates storing things in our wacky memory database
 exports.postscadmin = function(req,res){
-    var view     = {names: users};
-    var template = "{{#names}}<li>Name: {{name}} - ID: {{id}}</li>{{/names}}";
-    var compiled = Hogan.compile(template);
-    var html     = compiled.render(view);
-    rooms.push(req.param('room'));
+    var idx;
+    if (rooms.length > 0) idx = rooms.length;
+    else idx = 0;
+    rooms.push({id: idx, name: req.param('room')});
     res.render('sc-admin', {
-                title: 'Admin Page',
-                roomname: req.param('room'),
-                userlist: html
+                title: 'Admin Page'
     });
 };
 
@@ -34,12 +26,16 @@ exports.createroom = function(req,res){
       * We can do portions of pages like this with simple AJAX requests.
       */
     var name = req.param('name');
+    var view     = {sims: rooms};
+    var template = "<ul>{{#sims}}<li>Name: {{name}}</li>{{/sims}}</ul>";
+    var compiled = Hogan.compile(template);
+    var html     = compiled.render(view);
     // Note that the name: name part is option and just for demonstration
-    res.render('admin/createroom', {name: name});
+    res.render('admin/createroom', {roomlist: html});
 }
 
 exports.manageusers = function(req,res){
     var page = req.param('page');
     // See above. We're rendering just a part of a page as a response to an AJAX request.
-    res.render('admin/manageusers', {page: page});
+    res.render('admin/manageusers');
 }
