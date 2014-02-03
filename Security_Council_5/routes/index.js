e2931ce9ce7bead2aff083013871ac0281bf4a1f
Ryan Brushett <ryanbrushett@gmail.com>
2014-02-03 09:59:20 -0330

    expanded some tabs

--- Security_Council_5/routes/index.js
+++ Security_Council_5/routes/index.js
@@ -19,51 +19,49 @@ exports.login = function(req, res) {
 
 exports.loginUser = function(req, res) {
 
-	  // pull the form variables off the request body
-	  var username = req.body.username;
-	  var password = req.body.password;
+          // pull the form variables off the request body
+          var username = req.body.username;
+          var password = req.body.password;
 
-	  var check = 0;
-	  var users = db.users
-	  var name;
-	  var pass;
-	  var permission;
-	  
-	  console.log(users.length);
-	  for(var i = 0; i < users.length; i++)
-	  {
-		name = users[i].UserName;
-		pass = users[i].Password;
-		permission = users[i].Position
+          var check = 0;
+          var users = db.users
+          var name;
+          var pass;
+          var permission;
+          
+          console.log(users.length);
+          for(var i = 0; i < users.length; i++){
+                name = users[i].UserName;
+                pass = users[i].Password;
+                permission = users[i].Position
 
-		if(name == username && pass == password) 
-			{
-				if(permission == "member" || permission == "ambassador")
-					check = 1;
-				
-				else
-					check = 2;
-			break;	
-			}
-	  }
+                if(name == username && pass == password) {
+                                if(permission == "member" || permission == "ambassador")
+                                        check = 1;
+                                
+                                else
+                                        check = 2;
+                    break;        
+              }
+          }
 
-	 if(check == 1){
+         if(check == 1){
 
-	   // Normal user
+           // Normal user
 
-	    res.render('chatroom', { title: 'Chatroom'});
-	  }
-	 
-	 else if (check == 2){
-		 //user is an admin
-		 // change to after join
-		 // res.render('sc-admin');
-		 
-		 res.render('chatroom', { title: 'Chatroom'});
-	 }
-		 
-	  else {
-	    // failure
-		  res.render('index');
-	  }
-	};
+            res.render('chatroom', { title: 'Chatroom'});
+          }
+         
+         else if (check == 2){
+                 //user is an admin
+                 // change to after join
+                 // res.render('sc-admin');
+                 
+                 res.render('chatroom', { title: 'Chatroom'});
+         }
+                 
+          else {
+            // failure
+                  res.render('index');
+          }
+        };
