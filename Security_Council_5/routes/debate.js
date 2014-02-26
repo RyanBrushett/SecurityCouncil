71156d326b67fd80c399150bf12df8b7108ff570
Whymarrh Whitby <whymarrh.whitby@mun.ca>
2014-02-26 07:55:58 -0330

    Fix for user.flag

--- Security_Council_5/routes/debate.js
+++ Security_Council_5/routes/debate.js
@@ -2,6 +2,8 @@ var db = require('../db');
 
 exports.view = function(req, res) {
     var simulation = db.simulations[req.params.id];
-    simulation.currentUser = db.users[req.session.userId];
+    var currentUser = db.users[req.session.userId];
+    currentUser.flag = undefined;
+    simulation.currentUser = currentUser;
     res.render('debate/index', simulation);
 };
