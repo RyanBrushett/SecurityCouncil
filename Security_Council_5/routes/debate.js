cbe620d09148df51cace1579bb5cad5b23a64870
Whymarrh Whitby <whymarrh.whitby@mun.ca>
2014-02-26 06:48:07 -0330

    Update debate view

--- Security_Council_5/routes/debate.js
+++ Security_Council_5/routes/debate.js
@@ -2,5 +2,6 @@ var db = require('../db');
 
 exports.view = function(req, res) {
     var simulation = db.simulations[req.params.id];
+    simulation.currentUser = db.users[req.session.userId];
     res.render('debate/index', simulation);
 };
