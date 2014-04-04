a210ed0bd187107a2d93e9ac0950839d51566a99
Ryan Brushett <ryanbrushett@gmail.com>
2014-04-03 22:47:07 -0230

    Added flag property to comment

--- Security_Council_5/models/comment.js
+++ Security_Council_5/models/comment.js
@@ -3,6 +3,7 @@ function Comment(options) {
     this.content = options.content;
     this.id = options.id;
     this.user = options.user;
+    this.flag = options.flag;
 };
 
 module.exports = Comment;
