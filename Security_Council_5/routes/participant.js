a0fe7ce386dfa0e09d729606c39380d94b742958
Daniel Cook <dac456@mun.ca>
2014-04-04 02:05:32 -0230

    wip

--- Security_Council_5/routes/participant.js
+++ Security_Council_5/routes/participant.js
@@ -111,7 +111,7 @@ exports.debateMotion = function(req, res) {
         content: 'New motion under debate!\n' + motion.body + '\n' + 'Moved by: ' + motion.mover.name + '\n',
         user: user
     });
-    db.helpers.setCommentFlag(simulation, newComment, simulation.chairperson);
+    db.helpers.setCommentFlag(simulation, newComment, user);
     res.send(200);
 };
 
